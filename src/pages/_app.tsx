7aded46a92347d74e4a005a1a38e209c425f9c8f
Gabriel Trompiz <gab.tc@icloud.com>
2021-01-23 23:09:00 -0600

    Path aliases and Sass 7-1 pattern

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import { AppProps } from 'next/app';
+import '@styles/main.scss';
+
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+
+export default App;
