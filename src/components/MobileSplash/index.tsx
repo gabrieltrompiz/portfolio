71334d8ef5dd4f6a42c7c218666ca08848614f08
Gabriel Trompiz <gab.tc@icloud.com>
2021-11-14 21:57:37 -0600

    Including <title> meta tag in mobile splash page

--- src/components/MobileSplash/index.tsx
+++ src/components/MobileSplash/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Head from 'next/head';
 
 const MobileSplash: React.FC = () => (
-  <div id="mobile-splash">
-    Please use the desktop version of this site. The mobile version is still in development.
-  </div>
+  <>
+    <Head>
+      <title>Gabriel Trompiz - Developer</title>
+    </Head>
+    <div id="mobile-splash">
+      Please use the desktop version of this site. The mobile version is still in development.
+    </div>
+  </>
 );
 
 export default MobileSplash;
